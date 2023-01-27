798216e72fde832fc185abd6fe19230753e13815
APRsrch23 <9X.2G@pcsb.org>
2023-01-27 10:14:09 -0500

    making sure the query actually works

--- src/results.js
+++ src/results.js
@@ -1,32 +1,34 @@
 'use strict'
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-  setDoc,
-  updateDoc,
-  doc,
-  serverTimestamp,
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    limit,
+    onSnapshot,
+    setDoc,
+    updateDoc,
+    doc,
+    serverTimestamp,
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 import { getFirebaseConfig } from './firebase-config.js';
 var q = query('sensorimotor');
+console.log(q)
 var arraysOutput = []
 function calculateAverage(array) {
     var total = 0;
     var count = 0;
 
-    array.forEach(function(item, index) {
+    array.forEach(function (item, index) {
         total += item;
         count++;
     });
 
     return total / count;
 }
+
 function getMeanAsynchronyMilliseconds(tapsData) {
     tapsData.forEach(time => {
         time += 200;
@@ -35,11 +37,14 @@ function getMeanAsynchronyMilliseconds(tapsData) {
     })
     return calculateAverage(tapsData)
 }
-for (var subject in q) {
+
+/*for (var subject in q) {
     let times = subject.tapsData;
     arraysOutput.push(getMeanAsynchronyMilliseconds(times))
 }
-arraysElement.innerHTML = arraysOutput;
+arraysElement.innerHTML = arraysOutput;*/
+
+arraysElement.innerHTML = q;
 
 var arraysElement = document.getElementById("arrays");
 const firebaseAppConfig = getFirebaseConfig();
